8298e18a3155b3e33484eda71a3d15d09fc92bc6
Lisa <ljenkin2@hotmail.com>
2012-12-13 00:44:28 -0500

    Initial Commit

---
+++ Dec11/Dec11AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  Dec11AppDelegate.h
+//  Dec11
+//
+//  Created by Lisa Jenkins on 12/13/12.
+//  Copyright (c) 2012 Lisa Jenkins. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface Dec11AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
